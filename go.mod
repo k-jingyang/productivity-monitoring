1ff8d2572ff4e763e2928bb7260227563201de20
Kwek Jing Yang <k.jingyang@gmail.com>
2021-03-21 00:07:11 +0800

    Expose num of cards in lists via prometheus endpoint

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/adlio/trello v1.8.0
 	github.com/joho/godotenv v1.3.0
+	github.com/prometheus/client_golang v1.10.0
 )
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/adlio/trello"
 	"github.com/joho/godotenv"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const PERSONAL_BOARD_TODAY = "PERSONAL_BOARD_TODAY"
@@ -22,35 +26,97 @@ const WORK_BOARD_WAITING = "WORK_BOARD_WAITING"
 const WORK_BOARD_INBOX = "WORK_BOARD_INBOX"
 const WORK_BOARD_DONE = "WORK_BOARD_DONE"
 
+var client *trello.Client
+
+type FetchListLength func() float64
+
+func MakeFetchListFunc(listEnvName string) FetchListLength {
+	listId := os.Getenv(listEnvName)
+	return func() float64 {
+		list, err := client.GetList(listId, trello.Defaults())
+		if err != nil {
+			log.Fatalln("Error in retrieving list with id:", listId, "err:", err)
+		}
+		cards, err := list.GetCards(trello.Defaults())
+		if err != nil {
+			log.Fatalln("Error in retrieving cards from list.", "err:", err)
+		}
+		return float64(len(cards))
+	}
+}
+
 func main() {
 	godotenv.Load()
 	apiKey := os.Getenv("API_KEY")
 	token := os.Getenv("TOKEN")
 
-	client := trello.NewClient(apiKey, token)
+	client = trello.NewClient(apiKey, token)
 
-	listEnvNames := []string{
-		PERSONAL_BOARD_TODAY,
-		PERSONAL_BOARD_TOMORROW,
-		PERSONAL_BOARD_THIS_WEEK,
-		PERSONAL_BOARD_WAITING,
-		PERSONAL_BOARD_THIS_WEEK,
-		PERSONAL_BOARD_DONE,
-		WORK_BOARD_TODAY,
-		WORK_BOARD_TOMORROW,
-		WORK_BOARD_THIS_WEEK,
-		WORK_BOARD_WAITING,
-		WORK_BOARD_THIS_WEEK,
-		WORK_BOARD_DONE,
-	}
+	configureTrelloMetrics()
 
-	listEnvNames = []string{PERSONAL_BOARD_TODAY}
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
 
-	for _, listEnvName := range listEnvNames {
-		list, _ := client.GetList(os.Getenv(listEnvName), trello.Defaults())
-		cards, _ := list.GetCards(trello.Defaults())
+func configureTrelloMetrics() {
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "personal_board_today_count",
+		Help: "The count of cards in the today list of the Productivity System (Personal) board",
+	}, MakeFetchListFunc(PERSONAL_BOARD_TODAY))
 
-		fmt.Println(listEnvName, "has", len(cards), "cards.")
-	}
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "personal_board_tomorrow_count",
+		Help: "The count of cards in the tomorrow list of the Productivity System (Personal) board",
+	}, MakeFetchListFunc(PERSONAL_BOARD_TOMORROW))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "personal_board_this_week_count",
+		Help: "The count of cards in the this week list of the Productivity System (Personal) board",
+	}, MakeFetchListFunc(PERSONAL_BOARD_THIS_WEEK))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "personal_board_waiting_count",
+		Help: "The count of cards in the waiting list of the Productivity System (Personal) board",
+	}, MakeFetchListFunc(PERSONAL_BOARD_WAITING))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "personal_board_inbox_count",
+		Help: "The count of cards in the inbox list of the Productivity System (Personal) board",
+	}, MakeFetchListFunc(PERSONAL_BOARD_INBOX))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "personal_board_done_count",
+		Help: "The count of cards in the done list of the Productivity System (Personal) board",
+	}, MakeFetchListFunc(PERSONAL_BOARD_DONE))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "work_board_today_count",
+		Help: "The count of cards in the today list of the Productivity System (Work) board",
+	}, MakeFetchListFunc(WORK_BOARD_TODAY))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "work_board_tomorrow_count",
+		Help: "The count of cards in the tomorrow list of the Productivity System (Work) board",
+	}, MakeFetchListFunc(WORK_BOARD_TOMORROW))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "work_board_this_week_count",
+		Help: "The count of cards in the this week list of the Productivity System (Work) board",
+	}, MakeFetchListFunc(WORK_BOARD_THIS_WEEK))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "work_board_waiting_count",
+		Help: "The count of cards in the waiting list of the Productivity System (Work) board",
+	}, MakeFetchListFunc(WORK_BOARD_WAITING))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "work_board_inbox_count",
+		Help: "The count of cards in the inbox list of the Productivity System (Work) board",
+	}, MakeFetchListFunc(WORK_BOARD_INBOX))
+
+	var _ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "work_board_done_count",
+		Help: "The count of cards in the done list of the Productivity System (Work) board",
+	}, MakeFetchListFunc(WORK_BOARD_DONE))
 
 }
